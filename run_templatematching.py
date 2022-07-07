0cd1ec88ccebf3cc731bf1445f78d9154018f0a0
zoekrauss <zkrauss@uw.edu>
2022-07-07 15:15:48 -0700

    new templates

--- run_templatematching.py
+++ run_templatematching.py
@@ -12,15 +12,17 @@ start = datetime.now()
 # Read in templates
 tribe = eqcorrscan.core.match_filter.tribe.read_tribe('growclust_templates_sep2017.tgz')
 
+print('Read in the templates!')
+
 # Let's just use the first template as a test
 # tribe = eqcorrscan.core.match_filter.tribe.Tribe(templates = tribe[0])
 
 # Now detect!
 client = Client('IRIS')
 t1 = obspy.UTCDateTime("2017-09-01")
-t2 = obspy.UTCDateTime("2017-09-30")
+t2 = obspy.UTCDateTime("2017-09-01-01")
 
-party = tribe.client_detect(client,t1,t2,threshold=8, threshold_type='MAD',trig_int=1,save_progress=True)
+party = tribe.client_detect(client,t1,t2,threshold=6, threshold_type='MAD',trig_int=1,save_progress=False,process_cores=4)
 
 party.write('detections_sep2017')
 
