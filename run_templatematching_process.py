685df26258433b7c300594f5a270ef99181b3535
Ubuntu <adminuser@zoe-xcorr-vm.knzj0q34jqfexacekdd5x3j0jh.xx.internal.cloudapp.net>
2022-07-15 22:11:12 +0000

    corrected process-wise parallelization

--- run_templatematching_process.py
+++ run_templatematching_process.py
@@ -15,6 +15,7 @@ from timeit import default_timer as timer
 import pandas as pd
 from multiprocessing import Pool, cpu_count, Process
 import os
+import multiprocessing
 
 def day_detect(day1):
     tribe = eqcorrscan.core.match_filter.tribe.read_tribe('growclust_templates_sep2017.tgz')
@@ -38,15 +39,24 @@ def day_detect(day1):
 
 def main():
 
-    t1 = datetime(2019,9,1)
-    t2 = datetime(2019,9,4)
+    start = timer()
+
+    t1 = datetime(2017,9,1)
+    t2 = datetime(2018,9,30)
     time_bins = pd.to_datetime(np.arange(t1,t2,pd.Timedelta(1,'days')))
 
     for i in range(len(time_bins)):
-        p=Process(target=day_detect,args=(time_bins[i],))
+        p=multiprocessing.Process(target=day_detect,args=(time_bins[i],))
         # Pin created processes in a round-robin following https://sorami-chi.hateblo.jp/entry/2016/04/29/000000
-        os.system("taskset -p -c %d %d" % ((i % os.cpu_count()), p.pid))
+        print(i % os.cpu_count())
+        print(os.cpu_count())
+        print(os.getpid())
+        os.system("taskset -p -c %d %d" % ((i % os.cpu_count()), os.getpid()))
         p.start()
+    
+    end = timer()
+
+    print(f'elapsed time: {end - start}')
 
 
 if __name__ == '__main__':
